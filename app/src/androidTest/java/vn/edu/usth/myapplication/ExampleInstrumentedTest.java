a0bfe75b3ec2a2b144c81f4bb0e9b4448081423c
wikiepeidia <phamtheminh2409@gmail.com>
2025-10-01 16:39:02 +0700

    UPDATE NEW LOGIN SCREEN

--- app/src/androidTest/java/vn/edu/usth/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/vn/edu/usth/myapplication/ExampleInstrumentedTest.java
@@ -1,22 +1,22 @@
 /*
- * Copyright (c) 2025 Pham The Minh
+ * Copyright (c) 2025 Android project OpenVision API
  * All rights reserved.
  * Project: My Application
  * File: ExampleInstrumentedTest.java
- * Last Modified: 26/9/2025 3:10
+ * Last Modified: 1/10/2025 4:38
  */
 
 package vn.edu.usth.myapplication;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
--- app/src/main/java/vn/edu/usth/myapplication/PhotoAdapter.java
+++ app/src/main/java/vn/edu/usth/myapplication/PhotoAdapter.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2025 Pham The Minh
+ * Copyright (c) 2025 Android project OpenVision API
  * All rights reserved.
  * Project: My Application
  * File: PhotoAdapter.java
- * Last Modified: 26/9/2025 9:38
+ * Last Modified: 1/10/2025 4:38
  */
 
 package vn.edu.usth.myapplication;
